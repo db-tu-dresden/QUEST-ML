97997ed392a20c4403b0bb095d343104d4c81b57
Erik Nikulski <erik@nikulski.me>
2023-06-28 13:20:33 +0200

    Add __repr__ method to queue

--- system/queue.py
+++ system/queue.py
@@ -9,3 +9,7 @@ class Queue(simpy.Store):
         super().__init__(env, capacity)
         self.name = name
         self.data = data
+
+    def __repr__(self):
+        cls = self.__class__.__name__
+        return f'{cls}(data={self.data!r}, env={self._env!r}, capacity={self._capacity!r}, name={self.name!r})'
