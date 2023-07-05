d24629003e0655deaf1f71cc4e958fe8545571f0
Erik Nikulski <erik@nikulski.me>
2023-07-05 13:38:16 +0200

    Cast negative delays to zero

--- system/environment.py
+++ system/environment.py
@@ -1,7 +1,16 @@
+from typing import Optional, Any
+
 import simpy
+from simpy import Timeout
+from simpy.core import SimTime
 
 
 class Environment(simpy.Environment):
     def __init__(self):
         super().__init__()
         self.system = None
+
+    def timeout(self, delay: SimTime = 0, value: Optional[Any] = None) -> Timeout:
+        if delay < 0:
+            delay = 0
+        return super().timeout(delay, value)
