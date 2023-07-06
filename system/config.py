4475cd48fdc7bdcb610bd2e7fb22e0509d25a2e2
Erik Nikulski <erik@nikulski.me>
2023-07-06 16:12:11 +0200

    Add __setitem__ methode to config

--- system/config.py
+++ system/config.py
@@ -66,6 +66,9 @@ class Config:
     def __getitem__(self, item):
         return self.data[item]
 
+    def __setitem__(self, key, value):
+        self.data[key] = value
+
 
 def main():
     c = Config(path='../config.yaml')
