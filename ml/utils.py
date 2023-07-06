09188e4cafc8ed17fa77fbd78ba1fe5fda8a4284
Erik Nikulski <erik@nikulski.me>
2023-07-06 14:50:26 +0200

    Add custom arguments to optional contextmanager

--- ml/utils.py
+++ ml/utils.py
@@ -2,9 +2,9 @@ from contextlib import contextmanager
 
 
 @contextmanager
-def optional(condition, context_manager):
+def optional(condition, context_manager, *args, **kwargs):
     if condition:
-        with context_manager():
+        with context_manager(*args, **kwargs):
             yield
     else:
         yield
