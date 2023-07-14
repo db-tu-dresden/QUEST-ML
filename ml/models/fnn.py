6aca47e71b471646f3996f40b82485d257cc6a67
Erik Nikulski <erik@nikulski.me>
2023-07-14 16:58:27 +0200

    Add second FNN model

--- ml/models/fnn.py
+++ ml/models/fnn.py
@@ -18,3 +18,22 @@ class FNN(Model):
             out = self.activation(layer(out))
         out = self.out_layer(out)
         return out
+
+
+class FNN2(Model):
+    def __init__(self, input_size: int, hidden_size: int, output_size: int, hidden_layers: int = 2):
+        super().__init__()
+
+        self.model = nn.Sequential()
+        self.model.add_module('dense1', nn.Linear(input_size, hidden_size))
+        self.model.add_module('act1', nn.ReLU())
+
+        for i in range(hidden_layers):
+            self.model.add_module(f'dense{i + 2}', nn.Linear(hidden_size, hidden_size))
+            self.model.add_module(f'act{i + 2}', nn.ReLU())
+
+        self.model.add_module(f'dense{hidden_layers + 2}',  nn.Linear(hidden_size, output_size))
+        self.model.add_module(f'act{hidden_layers + 2}', nn.Sigmoid())
+
+    def forward(self, x: torch.Tensor):
+        return self.model(x)
