f955cb3077da88f950b70c82da2bbea7c4c75997
Erik Nikulski <erik@nikulski.me>
2023-07-19 18:26:06 +0200

    Set data to be difference to next step

--- ml/data.py
+++ ml/data.py
@@ -29,7 +29,14 @@ class ProcessDataset(Dataset):
     def __getitem__(self, item):
         jobs = sorted(self.da['job'].data)
 
+        diff = self.da.sel(job=jobs)[item + 1].to_numpy() - self.da.sel(job=jobs)[item].to_numpy()
+
         dist_source = self.da.sel(job=jobs)[item].to_numpy()
-        dist_target = self.da.sel(job=jobs)[item].to_numpy()
+        dist_source[0] = diff[0]
+        dist_source[-1].fill(0)
+
+        dist_target = self.da.sel(job=jobs)[item + 1].to_numpy()
+        dist_target[0].fill(0)
+        dist_target[-1] = diff[-1]
 
         return torch.tensor(dist_source, dtype=torch.float), torch.tensor(dist_target, dtype=torch.float)
