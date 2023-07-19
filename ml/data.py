08937aaa29f7ba894737b0b14748171b1cad8830
Erik Nikulski <erik@nikulski.me>
2023-07-19 18:26:37 +0200

    Remove window size

--- ml/data.py
+++ ml/data.py
@@ -6,10 +6,9 @@ import xarray as xr
 
 
 class ProcessDataset(Dataset):
-    def __init__(self, da: xr.DataArray, scaling_factor: int = 1, window_size: int = 10):
+    def __init__(self, da: xr.DataArray, scaling_factor: int = 1):
         self.da = da
         self.scaling_factor = scaling_factor
-        self.window_size = window_size
 
         self.orig_da = self.da
         self.da = self.da[::scaling_factor]
