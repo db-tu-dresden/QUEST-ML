f71ec505393265d4ac1c1cdc9051c759d1690eb6
Erik Nikulski <erik@nikulski.me>
2023-07-22 23:36:37 +0200

    Update base model

--- ml/models/base.py
+++ ml/models/base.py
@@ -5,6 +5,15 @@ from ml import ddp
 
 
 class Model(nn.Module):
+
+    @staticmethod
+    def add_args(parser):
+        pass
+
+    @classmethod
+    def build_model(cls, args):
+        pass
+
     def save(self, path: str):
         if ddp.is_main_process():
             torch.save(self.state_dict(), path)
