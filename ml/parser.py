ed489071c278ebd94f3f018c7d2fd67bb752c797
Erik Nikulski <erik@nikulski.me>
2023-07-23 17:12:41 +0200

    Override help method to provide architecture argument options if architecture is provided

--- ml/parser.py
+++ ml/parser.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 
 
 def add_model_args(parser):
@@ -12,9 +13,39 @@ def add_model_args(parser):
     return group
 
 
+def override_format_help(parser):
+    old_format_help = parser.format_help
+
+    def format_help():
+        msg = old_format_help()
+        args = sys.argv[1:]
+
+        ident = None
+
+        if '--arch' in args:
+            ident = '--arch'
+        elif '-a' in args:
+            ident = '-a'
+
+        if ident is None:
+            return msg
+
+        import ml
+        arch = args[args.index(ident) + 1]
+        parser_id = arch + '_parser'
+
+        if parser_id not in vars(ml.models):
+            return msg
+
+        return vars(ml.models)[parser_id + '_parser'].format_help()
+
+    parser.format_help = format_help
+
+
 parser = argparse.ArgumentParser(description='ML infrastructure for training and testing models')
 parser.add_argument('-g', '--gpu', action='store_true', help='Enable GPU usage')
 parser.add_argument('-n', '--n_gpus', type=int, metavar='N', help='How many GPUs to use.If not set, all GPUs are used')
 parser.add_argument('--wandb', type=bool, default=True, help='Whether to use wandb is used for logging.')
 
 add_model_args(parser)
+override_format_help(parser)
