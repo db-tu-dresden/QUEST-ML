6bc68b3e0c43e20ad70c4dead17c2394002dc1b5
Erik Nikulski <erik@nikulski.me>
2023-07-26 15:15:12 +0200

    Enable gap of multiple steps in data input-output pairs

--- ml/data.py
+++ ml/data.py
@@ -6,9 +6,10 @@ import xarray as xr
 
 
 class ProcessDataset(Dataset):
-    def __init__(self, da: xr.DataArray, scaling_factor: int = 1):
+    def __init__(self, da: xr.DataArray, scaling_factor: int = 1, prediction_gap: int = 1):
         self.da = da
         self.scaling_factor = scaling_factor
+        self.offset = prediction_gap
 
         self.orig_da = self.da
         self.da = self.da[::scaling_factor]
@@ -23,18 +24,18 @@ class ProcessDataset(Dataset):
         return self[0][0].shape
 
     def __len__(self):
-        return len(self.da) - 1     # -1 because the next step is the label; therefore the last step is omitted
+        return len(self.da) - self.offset
 
     def __getitem__(self, item):
         jobs = sorted(self.da['job'].data)
 
-        diff = self.da.sel(job=jobs)[item + 1].to_numpy() - self.da.sel(job=jobs)[item].to_numpy()
+        diff = self.da.sel(job=jobs)[item + self.offset].to_numpy() - self.da.sel(job=jobs)[item].to_numpy()
 
         dist_source = self.da.sel(job=jobs)[item].to_numpy()
         dist_source[0] = diff[0]
         dist_source[-1].fill(0)
 
-        dist_target = self.da.sel(job=jobs)[item + 1].to_numpy()
+        dist_target = self.da.sel(job=jobs)[item + self.offset].to_numpy()
         dist_target[0].fill(0)
         dist_target[-1] = diff[-1]
 
