af231b71287961bb3dc380202b482e77ce599178
Erik Nikulski <erik@nikulski.me>
2023-07-28 23:47:57 +0200

    Add arguments to enable loading pretrained models

--- ml/models/encoder_decoder.py
+++ ml/models/encoder_decoder.py
@@ -1,5 +1,7 @@
 import argparse
 
+import torch
+
 from ml import Model, Config
 from ml.models import get_model_from_type
 
@@ -19,17 +21,25 @@ class EncoderDecoder(Model):
     @staticmethod
     def add_args(parser: argparse.ArgumentParser):
         parser.add_argument('--encoder', type=str, help='Encoder model name')
+        parser.add_argument('--encoder_state_path', type=str,
+                            help='Path to the state dict of a pretrained encoder model')
         parser.add_argument('--decoder', type=str, help='Decoder model name')
+        parser.add_argument('--decoder_state_path', type=str,
+                            help='Path to the state dict of a pretrained decoder model')
 
     @classmethod
     def build_model(cls, config: Config):
         encoder_type = config['encoder'] if 'encoder' in config else None
         encoder_model = get_model_from_type(encoder_type, config)
         encoder = encoder_model.build_model(config)
+        if config['encoder_state_path']:
+            encoder.load_state_dict(torch.load(config['encoder_state_path']))
 
         decoder_type = config['decoder'] if 'decoder' in config else None
         decoder_model = get_model_from_type(decoder_type, config)
         decoder = decoder_model.build_model(config)
+        if config['decoder_state_path']:
+            encoder.load_state_dict(torch.load(config['decoder_state_path']))
 
         return cls(encoder, decoder)
 
@@ -51,21 +61,33 @@ class EncoderFusionDecoder(Model):
     @staticmethod
     def add_args(parser: argparse.ArgumentParser):
         parser.add_argument('--encoder', type=str, help='Encoder model name')
+        parser.add_argument('--encoder_state_path', type=str,
+                            help='Path to the state dict of a pretrained encoder model')
         parser.add_argument('--fusion', type=str, help='Fusion model name')
+        parser.add_argument('--fusion_state_path', type=str,
+                            help='Path to the state dict of a pretrained fusion model')
         parser.add_argument('--decoder', type=str, help='Decoder model name')
+        parser.add_argument('--decoder_state_path', type=str,
+                            help='Path to the state dict of a pretrained decoder model')
 
     @classmethod
     def build_model(cls, config: Config):
         encoder_type = config['encoder'] if 'encoder' in config else None
         encoder_model = get_model_from_type(encoder_type, config)
         encoder = encoder_model.build_model(config)
+        if config['encoder_state_path']:
+            encoder.load_state_dict(torch.load(config['encoder_state_path']))
 
         fusion_type = config['fusion'] if 'fusion' in config else None
         fusion_model = get_model_from_type(fusion_type, config)
         fusion = fusion_model.build_model(config)
+        if config['fusion_state_path']:
+            encoder.load_state_dict(torch.load(config['fusion_state_path']))
 
         decoder_type = config['decoder'] if 'decoder' in config else None
         decoder_model = get_model_from_type(decoder_type, config)
         decoder = decoder_model.build_model(config)
+        if config['decoder_state_path']:
+            encoder.load_state_dict(torch.load(config['decoder_state_path']))
 
         return cls(encoder, fusion, decoder)
