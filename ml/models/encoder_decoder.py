f34d2d3bb8c779cafc5bd108b4ede8f96b432659
Erik Nikulski <erik@nikulski.me>
2023-07-28 23:36:09 +0200

    Add Encoder Fusion Decoder Model

--- ml/models/encoder_decoder.py
+++ ml/models/encoder_decoder.py
@@ -30,3 +30,37 @@ class EncoderDecoder(Model):
         decoder = get_model_from_type(decoder_type, config)
 
         return cls(encoder, decoder)
+
+
+class EncoderFusionDecoder(Model):
+    def __init__(self, encoder: Model, fusion: Model, decoder: Model):
+        super().__init__()
+        self.encoder = encoder
+        self.fusion = fusion
+        self.decoder = decoder
+
+    def forward(self, x):
+        out = x
+        out = self.encoder(out)
+        out = self.fusion(out)
+        out = self.decoder(out)
+        return out
+
+    @staticmethod
+    def add_args(parser: argparse.ArgumentParser):
+        parser.add_argument('--encoder', type=str, help='Encoder model name')
+        parser.add_argument('--fusion', type=str, help='Fusion model name')
+        parser.add_argument('--decoder', type=str, help='Decoder model name')
+
+    @classmethod
+    def build_model(cls, config: Config):
+        encoder_type = config['encoder'] if 'encoder' in config else None
+        encoder = get_model_from_type(encoder_type, config)
+
+        fusion_type = config['fusion'] if 'fusion' in config else None
+        fusion = get_model_from_type(fusion_type, config)
+
+        decoder_type = config['decoder'] if 'decoder' in config else None
+        decoder = get_model_from_type(decoder_type, config)
+
+        return cls(encoder, fusion, decoder)
