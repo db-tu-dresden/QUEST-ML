24f65e1b0991ecb3414cedf010c78b0c9b2948c2
Erik Nikulski <erik@nikulski.me>
2023-08-17 13:33:34 +0200

    Always log model

--- ml/trainer.py
+++ ml/trainer.py
@@ -192,7 +192,7 @@ class Trainer:
             self.config.update(config)
 
         with self.logger(self.config):
-            self.logger.log(self.model)
+            self.logger.log(self.model, verbose=False)
             if self.config['wandb_watch_model']:
                 self.logger.watch(self.model, self.criterion, log='all')
             for epoch in range(self.config['epochs']):
