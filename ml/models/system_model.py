5c7c52cf8281274dc97a30bd217f04b899ee22fb
Erik Nikulski <erik@nikulski.me>
2023-08-22 15:42:39 +0200

    Update fusion model hidden size

--- ml/models/system_model.py
+++ ml/models/system_model.py
@@ -427,7 +427,7 @@ def fusion_model(cfg: Config, prefix: str = 'encoder_fusion_'):
     cfg[f'{prefix}model_input_size'] = cfg[f'{prefix}model_input_size'] \
         if f'{prefix}model_input_size' in cfg else 2**int(cfg[f'{prefix}bidirectional']) * cfg[f'{prefix}hidden_size']
     cfg[f'{prefix}model_hidden_size'] = cfg[f'{prefix}model_hidden_size'] \
-        if f'{prefix}model_hidden_size' in cfg else cfg[f'{prefix}model_input_size']
+        if f'{prefix}model_hidden_size' in cfg else cfg['system_embedding_size']
     cfg[f'{prefix}model_hidden_layers'] = cfg[f'{prefix}model_hidden_layers'] \
         if f'{prefix}model_hidden_layers' in cfg else 1
     cfg[f'{prefix}model_output_size'] = cfg[f'{prefix}model_output_size'] \
