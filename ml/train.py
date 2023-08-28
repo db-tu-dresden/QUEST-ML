b5af849429351fd248d891617afafbb1a5f81604
Erik Nikulski <erik@nikulski.me>
2023-08-28 12:44:10 +0200

    Move training script into ml package

---
+++ ml/train.py
@@ -0,0 +1,19 @@
+import os
+
+from ml import seed, Config, Parser, Trainer
+from ml.models import build_model
+
+
+def run():
+    seed()
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    config = Config(os.path.join(cwd, 'config.yaml'))
+
+    parser = Parser(config)
+    args = parser.parse_args()
+
+    config.update_from_args(args)
+
+    model = build_model(config)
+
+    Trainer.run(config, model)
--- train.py
+++ train.py
@@ -1,20 +1,6 @@
-from ml import Config, Trainer, Parser, seed
-from ml.models import build_model
+import sys
 
-
-def run():
-    seed()
-    config = Config('ml/config.yaml')
-
-    parser = Parser(config)
-    args = parser.parse_args()
-
-    config.update_from_args(args)
-
-    model = build_model(config)
-
-    Trainer.run(config, model)
-
+import ml.train
 
 if __name__ == '__main__':
-    run()
+    sys.exit(ml.train.run())
