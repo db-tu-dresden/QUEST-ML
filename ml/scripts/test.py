6ab9e6ddbc4106c74d392580c40ee0d29f4db914
Erik Nikulski <erik@nikulski.me>
2023-08-30 11:55:17 +0200

    Add test script

---
+++ ml/scripts/test.py
@@ -0,0 +1,27 @@
+import os
+
+from ml import seed, Config, Parser, Trainer
+from ml.models import build_model
+
+
+def run():
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    config = Config(os.path.join(cwd, '../config.yaml'))
+
+    seed(config['seed'])
+
+    parser = Parser(config)
+    args = parser.parse_args()
+
+    config.update_from_args(args)
+
+    model = build_model(config)
+
+    train_data, valid_data, test_data = Trainer.get_datasets_from_path(config['data_path'],
+                                                                       config['scaling_factor'],
+                                                                       config['reduction_factor'],
+                                                                       config['offset'],
+                                                                       config['only_process'],
+                                                                       config['pickle_file_name'])
+    trainer = Trainer(config, model, train_data, valid_data, test_data)
+    trainer.test()
---
+++ test.py
@@ -0,0 +1,6 @@
+import sys
+
+import ml.scripts.test
+
+if __name__ == '__main__':
+    sys.exit(ml.scripts.test.run())
