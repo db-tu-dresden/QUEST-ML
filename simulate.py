cb27fe3db21a5fae0c7e2a5daf1f781ae7007849
Erik Nikulski <erik@nikulski.me>
2023-09-22 16:13:16 +0200

    Add argument to toggle plotting

--- simulate.py
+++ simulate.py
@@ -10,6 +10,8 @@ parser.add_argument('-p', '--path', help='Path where a config.yaml describing th
                                          'a graph_description.note describing the process graph lie.')
 parser.add_argument('-u', '--until', metavar='N', type=int, help='Number of steps until which '
                                                                  'the simulation should run')
+parser.add_argument('--plot', default=True, action=argparse.BooleanOptionalAction,
+                    help='Enable graph and process distribution plotting')
 
 
 @log_runtime
@@ -20,7 +22,8 @@ def run(args):
         text = f.read()
 
     notation = Notation.parse(text)
-    notation.draw(os.path.join(base_path, 'graph.png'))
+    if args.plot:
+        notation.draw(os.path.join(base_path, 'graph.png'))
 
     config = Config(os.path.join(base_path, 'config.yaml'))
 
@@ -28,7 +31,8 @@ def run(args):
 
     system = System(config, notation, env=env)
     system.run(args.until)
-    system.logger.plot(path=os.path.join(base_path, 'dist.png'))
+    if args.plot:
+        system.logger.plot(path=os.path.join(base_path, 'dist.png'))
 
 
 if __name__ == '__main__':
