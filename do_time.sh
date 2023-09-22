562301e370af3be699e4f8d952c82d18ace32c2d
Erik Nikulski <erik@nikulski.me>
2023-09-22 15:04:37 +0200

    Add script to measure runtimes

---
+++ do_time.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+while getopts ":n:c:" opt; do
+  case $opt in
+    n) N="$OPTARG"
+    ;;
+    c) COMMAND="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    exit 1
+    ;;
+  esac
+
+  case $OPTARG in
+    -*) echo "Option $opt needs a valid argument"
+    exit 1
+    ;;
+  esac
+done
+
+
+RESULTS=./time_results.txt
+touch $RESULTS
+truncate -s 0  $RESULTS
+
+#COMMAND=python3 create_data.py --path './save/-/'
+
+printf "%s\n\n" "$COMMAND" | tee $RESULTS
+
+for i in {0..10};
+do
+  { time "$COMMAND"; } 2>&1 | grep real | tee $RESULTS
+done
