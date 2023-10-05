53be1044c404329b05d0af2f53171e04369f2c13
Erik Nikulski <erik@nikulski.me>
2023-10-05 12:01:45 +0200

    Add simulation execution functions

--- system/__init__.py
+++ system/__init__.py
@@ -1,3 +1,10 @@
+import os
+from datetime import datetime
+
+import numpy as np
+import torch
+
+from notation import Notation
 from system.config import Config
 from system.environment import Environment
 from system.job import Job
@@ -6,3 +13,50 @@ from system.process import Process, ArrivalProcess, ExitProcess
 from system.queue import Queue
 from system.system import System
 
+
+def simulate(notation_path: str, save_path: str, plot: bool, until: int):
+    with open(notation_path) as f:
+        notation_string = f.read()
+
+    notation = Notation.parse(notation_string)
+    if plot:
+        notation.draw(os.path.join(save_path, 'graph.png'))
+
+    config = Config(os.path.join(save_path, 'config.yaml'))
+
+    env = Environment()
+
+    system = System(config, notation, env=env)
+    system.run(until)
+    if plot:
+        system.logger.plot(path=os.path.join(save_path, 'dist.png'))
+
+
+def simulate_from_state(config: Config, notation_path: str, state: torch.Tensor, steps: int, k: int = 1, verbose=False):
+    with open(notation_path) as f:
+        text = f.read()
+    notation = Notation.parse(text)
+
+    final_states = []
+    for i in range(k):
+        env = Environment()
+        config['randomSeed'] = int(''.join(str(el) for el in datetime.now().timestamp().as_integer_ratio()))
+        sys = System(config, notation, env=env)
+        sys.set_state(state)
+        sys.run(steps)
+
+        final_state = sys.logger.get_current_state()
+        final_states.append(final_state)
+        if verbose:
+            print(f'Final state in run {i + 1} of {k} is:\n'
+                  f'{final_state}\n')
+
+    final_states = np.stack(final_states)
+    mean = final_states.mean(axis=0)
+    std = final_states.std(axis=0)
+    print(f'Mean is: \n'
+          f'{mean}\n')
+    print(f'Standard Deviation is: \n'
+          f'{std}\n\n')
+
+    return list(zip([steps] * k, [torch.tensor(state) for state in final_states]))
