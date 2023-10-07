5553ff0042bcdad602daea93228c4d24efbb597e
Erik Nikulski <erik@nikulski.me>
2023-10-07 14:26:18 +0200

    Align simulate_from_state

--- system/__init__.py
+++ system/__init__.py
@@ -14,6 +14,19 @@ from system.queue import Queue
 from system.system import System
 
 
+def print_run_stats(run_data: list, k: int):
+    print(f'Ran simulation {k} times')
+    print(f'Steps: {np.mean([elem["steps"] for elem in run_data])} '
+          f'± {np.std([elem["steps"] for elem in run_data])}')
+    print(f'Runtime: {np.mean([elem["runtime"] for elem in run_data])} sec '
+          f'± {np.std([elem["runtime"] for elem in run_data])} sec')
+    print(f'Final states: \n'
+          f'Mean\n'
+          f'{np.mean([elem["final_state"] for elem in run_data], axis=0)}\n'
+          f'Std\n'
+          f'{np.std([elem["final_state"] for elem in run_data], axis=0)}\n')
+
+
 def simulate(notation_path: str, save_path: str, plot: bool, until: int):
     with open(notation_path) as f:
         notation_string = f.read()
@@ -32,34 +45,34 @@ def simulate(notation_path: str, save_path: str, plot: bool, until: int):
         sys.logger.plot(path=os.path.join(save_path, 'dist.png'))
 
 
-def simulate_from_state(config: Config, notation_path: str, state: torch.Tensor, steps: int, k: int = 1, verbose=False):
+def simulate_from_state(config: Config, notation_path: str, initial_state: torch.Tensor, steps: int, k: int = 1,
+                        verbose=False, vary_random_seed: bool = True):
     with open(notation_path) as f:
         text = f.read()
     notation = Notation.parse(text)
 
-    final_states = []
+    run_data = []
+
     for i in range(k):
         env = Environment()
-        config['randomSeed'] = int(''.join(str(el) for el in datetime.now().timestamp().as_integer_ratio()))
+        if vary_random_seed:
+            config['randomSeed'] = int(''.join(str(el) for el in datetime.now().timestamp().as_integer_ratio()))
+
         sys = System(config, notation, env=env)
-        sys.set_state(state)
+        sys.set_state(initial_state)
         sys.run(steps)
 
-        final_state = sys.logger.get_state()
-        final_states.append(final_state)
-        if verbose:
-            print(f'Final state in run {i + 1} of {k} is:\n'
-                  f'{final_state}\n')
+        run_data.append({
+            'steps': sys.logger.get_steps(),
+            'runtime': sys.logger.get_runtime(),
+            'final_state': sys.logger.get_state(),
+            'job_arrivals': sys.logger.get_job_arrivals()
+        })
 
-    final_states = np.stack(final_states)
-    mean = final_states.mean(axis=0)
-    std = final_states.std(axis=0)
-    print(f'Mean is: \n'
-          f'{mean}\n')
-    print(f'Standard Deviation is: \n'
-          f'{std}\n\n')
+    if verbose:
+        print_run_stats(run_data, k)
 
-    return list(zip([steps] * k, [torch.tensor(state) for state in final_states]))
+    return run_data
 
 
 def simulate_to_target(config: Config, notation_path: str, initial_state: torch.Tensor, target_dist: torch.Tensor,
@@ -92,15 +105,6 @@ def simulate_to_target(config: Config, notation_path: str, initial_state: torch.
         })
 
     if verbose:
-        print(f'Ran simulation {k} times')
-        print(f'Steps: {np.mean([elem["steps"] for elem in run_data])} '
-              f'± {np.std([elem["steps"] for elem in run_data])}')
-        print(f'Runtime: {np.mean([elem["runtime"] for elem in run_data])} '
-              f'± {np.std([elem["runtime"] for elem in run_data])}')
-        print(f'Final states: \n'
-              f'Mean\n'
-              f'{np.mean([elem["final_state"] for elem in run_data], axis=0)}\n'
-              f'Std\n'
-              f'{np.std([elem["final_state"] for elem in run_data], axis=0)}')
+        print_run_stats(run_data, k)
 
     return run_data
