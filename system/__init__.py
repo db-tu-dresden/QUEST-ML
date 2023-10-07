e32e6c9e59966e8df1d44077fc1cd18546bb8453
Erik Nikulski <erik@nikulski.me>
2023-10-07 14:06:01 +0200

    Run multiple times

--- system/__init__.py
+++ system/__init__.py
@@ -62,7 +62,8 @@ def simulate_from_state(config: Config, notation_path: str, state: torch.Tensor,
     return list(zip([steps] * k, [torch.tensor(state) for state in final_states]))
 
 
-def simulate_to_target(config: Config, notation_path: str, initial_state: torch.Tensor, target_dist: torch.Tensor):
+def simulate_to_target(config: Config, notation_path: str, initial_state: torch.Tensor, target_dist: torch.Tensor,
+                       k: int = 1, verbose: bool = False, vary_random_seed: bool = True):
     def break_on_target(process: ExitProcess):
         return all(target_dist[i] <= job_count for i, (_, job_count) in enumerate(process.job_dist.items()))
 
@@ -71,9 +72,35 @@ def simulate_to_target(config: Config, notation_path: str, initial_state: torch.
 
     notation = Notation.parse(notation_string)
 
-    env = Environment()
+    run_data = []
 
-    sys = System(config, notation, env=env)
-    sys.set_break_condition(break_on_target)
-    sys.set_state(initial_state)
-    sys.run()
+    for i in range(k):
+        env = Environment()
+        if vary_random_seed:
+            config['randomSeed'] = int(''.join(str(el) for el in datetime.now().timestamp().as_integer_ratio()))
+
+        sys = System(config, notation, env=env)
+        sys.set_break_condition(break_on_target)
+        sys.set_state(initial_state)
+        sys.run()
+
+        run_data.append({
+            'steps': sys.logger.get_steps(),
+            'runtime': sys.logger.get_runtime(),
+            'final_state': sys.logger.get_state(),
+            'job_arrivals': sys.logger.get_job_arrivals()
+        })
+
+    if verbose:
+        print(f'Ran simulation {k} times')
+        print(f'Steps: {np.mean([elem["steps"] for elem in run_data])} '
+              f'± {np.std([elem["steps"] for elem in run_data])}')
+        print(f'Runtime: {np.mean([elem["runtime"] for elem in run_data])} '
+              f'± {np.std([elem["runtime"] for elem in run_data])}')
+        print(f'Final states: \n'
+              f'Mean\n'
+              f'{np.mean([elem["final_state"] for elem in run_data], axis=0)}\n'
+              f'Std\n'
+              f'{np.std([elem["final_state"] for elem in run_data], axis=0)}')
+
+    return run_data
